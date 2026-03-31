cced496ddf99ba0e0be702f00138cb72e805b241
James Patrick Mobley <pat_mobley@Jamess-MacBook-Air.local>
2026-03-31 13:56:38 -0700

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -50,6 +51,22 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-VH80XCS2T1"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-VH80XCS2T1');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
